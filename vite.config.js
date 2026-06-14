b8ffc8d970588cedc79cac771221cb48f97474be
senasam <senasam@gmail.com>
2026-06-14 07:43:28 -0400

    fix 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mi-pagina-web/",
+  base: process.env.GITHUB_PAGES === "true" ? "/mi-pagina-web/" : "/",
   plugins: [react()],
 });
